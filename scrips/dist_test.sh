59f46f7fb73ad83b240436d7e5f2cd47ee3153f8
Yamamoto <dyamamoto@bapm.com>
2019-08-05 14:54:23 -0400

    fix path

--- scrips/dist_test.sh
+++ scrips/dist_test.sh
@@ -7,4 +7,4 @@ CHECKPOINT=$2
 GPUS=$3
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS \
-    /kaggle-imaterialist/mmdetection/tools/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
+    /svs/mmdetection/tools/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
