74a9b1ad6be50ee29374691164ccd595d0a14619
Yamamoto <dyamamoto@bapm.com>
2019-08-05 17:29:44 -0400

    fix path5

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ def group2mmdetection(group: dict) -> dict:
     height, width = group['Height'].max(), group['Width'].max()
     def convert(x):
         if x == np.nan: return x
-        else: return kaggle2coco(list(map(int, x.split())), height, width))
+        else: return kaggle2coco(list(map(int, x.split())), height, width)
 
     rles = group['EncodedPixels'].apply(convert).tolist()
     rles = mutils.frPyObjects(rles, height, width)
