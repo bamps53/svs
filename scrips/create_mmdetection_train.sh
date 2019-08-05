1e5a87ed9f883c95c5cbe4440640321540cd6a3d
Yamamoto <dyamamoto@bapm.com>
2019-08-05 15:40:36 -0400

    fix path4

--- scrips/create_mmdetection_train.sh
+++ scrips/create_mmdetection_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PYTHONPATH=/svs python src/create_mmdetection_train.py \
+PYTHONPATH=/content/drive/My Drive/Severstal/svs python src/create_mmdetection_train.py \
     --annotation=/data/train.csv.zip \
     --root=/data/train \
     --output=/data/train_mmdetection.pkl
