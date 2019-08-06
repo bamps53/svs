a09c78b404a3bcf00d4d39e923b3090acea9dcc8
Yamamoto <dyamamoto@bapm.com>
2019-08-06 13:58:31 -0400

    fix split.sh

--- scrips/split.sh
+++ scrips/split.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PYTHONPATH=/svs python /src/split.py \
+PYTHONPATH=/svs python src/split.py \
     --annotation=/data/train_mmdetection.pkl \
     --train_output=/data/train_99_mmdetection.pkl \
     --val_output=/data/val_01_mmdetection.pkl
