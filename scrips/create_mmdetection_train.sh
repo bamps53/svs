779e7c97c853dea94fe7ab3a5c17b1ab59db0283
daisuke0530 <daisuke0530@gmail.com>
2019-08-05 22:39:38 -0400

    fix prune.py

--- scrips/create_mmdetection_train.sh
+++ scrips/create_mmdetection_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 PYTHONPATH=/svs python src/create_mmdetection_train.py \
-    --annotation=data/train.csv.zip \
+    --annotation=data/train.csv \
     --root=data/train_images \
     --output=data/train_mmdetection.pkl
