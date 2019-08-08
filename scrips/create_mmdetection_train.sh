b2cb6662cc6ca119abc7740c52b548e7b339e70c
daisuke0530 <daisuke0530@gmail.com>
2019-08-07 21:07:12 -0400

    fix kaggle path

--- scrips/create_mmdetection_train.sh
+++ scrips/create_mmdetection_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 PYTHONPATH=/svs python src/create_mmdetection_train.py \
-    --annotation=../input/severstal-steel-defect-detection/train.csv \
-    --root=../input/severstal-steel-defect-detection/train_images \
+    --annotation=/kaggle/input/severstal-steel-defect-detection/train.csv \
+    --root=/kaggle/input/severstal-steel-defect-detection/train_images \
     --output=data/train_mmdetection.pkl
