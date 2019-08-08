e0a2e74fbfd22d96734f99afacd28760e1cddac2
Yamamoto <dyamamoto@bapm.com>
2019-08-08 16:10:05 -0400

    add normal htc

--- configs/faster_rcnn_r50_fpn_1x.py
+++ configs/faster_rcnn_r50_fpn_1x.py
@@ -95,7 +95,7 @@ test_cfg = dict(
 )
 # dataset settings
 dataset_type = 'CustomDataset'
-data_root = '../input/'
+data_root = 'data/'
 work_root = 'data/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
