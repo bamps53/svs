e4711a30b5b1a1e0e789a58d5448a655e624c9d0
daisuke0530 <daisuke0530@gmail.com>
2019-08-07 21:44:48 -0400

    fix split.sh

--- scrips/split.sh
+++ scrips/split.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PYTHONPATH=/svs python src/split.py \
-    --annotation=/kaggle/input/severstal-steel-defect-detection/train_mmdetection.pkl \
+    --annotation=data/train_mmdetection.pkl \
     --train_output=data/train_9_mmdetection.pkl \
     --val_output=data/val_1_mmdetection.pkl \
     --n_splits=10
